8cc4020fe65e211d82ad8250a57e655a1eeb66f1
Ning Sun <sunng@about.me>
2016-03-12 16:26:50 +0800

    (feat) added dry-run option

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ extern crate clap;
 
 use std::process::exit;
 
-use clap::App;
+use clap::{App, ArgMatches};
 use semver::Identifier;
 
 mod config;
@@ -17,12 +17,15 @@ mod cmd;
 mod git;
 mod cargo;
 
-fn execute() -> Result<i32, error::FatalError> {
+fn execute(args: &ArgMatches) -> Result<i32, error::FatalError> {
+    let dry_run = args.occurrences_of("dry-run") > 0;
 
     // STEP 0: Check if working directory is clean
     if !try!(git::status()) {
         println!("Uncommitted changes detected, please commit before release");
-        return Ok(101);
+        if !dry_run {
+            return Ok(101);
+        }
     }
 
     // STEP 1: Read version from Cargo.toml and remove
@@ -39,28 +42,36 @@ fn execute() -> Result<i32, error::FatalError> {
     if version.is_prerelease() {
         version.pre.clear();
         let new_version_string = version.to_string();
-        try!(config::rewrite_cargo_version(&new_version_string));
-        // sync Cargo.toml and Cargo.lock
-        try!(cargo::update());
+        if !dry_run {
+            try!(config::rewrite_cargo_version(&new_version_string));
+            // sync Cargo.toml and Cargo.lock
+            try!(cargo::update());
+        }
 
         let commit_msg = format!("(cargo-release) version {}", new_version_string);
-        if !try!(git::commit_all(&commit_msg)) {
-            // commit failed, abort release
-            return Ok(102);
+        if !dry_run {
+            if !try!(git::commit_all(&commit_msg)) {
+                // commit failed, abort release
+                return Ok(102);
+            }
         }
     }
 
     // STEP 3: cargo publish
-    if !try!(cargo::publish()) {
-        return Ok(103);
+    if !dry_run {
+        if !try!(cargo::publish()) {
+            return Ok(103);
+        }
     }
 
     // STEP 4: Tag
     let current_version = version.to_string();
     let tag_message = format!("(cargo-release) version {}", current_version);
-    if !try!(git::tag(&current_version, &tag_message)) {
-        // tag failed, abort release
-        return Ok(104);
+    if !dry_run {
+        if !try!(git::tag(&current_version, &tag_message)) {
+            // tag failed, abort release
+            return Ok(104);
+        }
     }
 
     // STEP 5: bump version
@@ -68,30 +79,38 @@ fn execute() -> Result<i32, error::FatalError> {
     version.pre.push(Identifier::AlphaNumeric("pre".to_owned()));
     println!("Starting next development cycle {}", version);
     let updated_version_string = version.to_string();
-    try!(config::rewrite_cargo_version(&updated_version_string));
-    try!(cargo::update());
+    if !dry_run {
+        try!(config::rewrite_cargo_version(&updated_version_string));
+        try!(cargo::update());
+    }
     let commit_msg = format!("(cargo-release) start next development cycle {}",
                              updated_version_string);
-    if !try!(git::commit_all(&commit_msg)) {
-        return Ok(105);
+    if !dry_run {
+        if !try!(git::commit_all(&commit_msg)) {
+            return Ok(105);
+        }
     }
 
     // STEP 6: git push
-    if !try!(git::push()) {
-        return Ok(106);
+    if !dry_run {
+        if !try!(git::push()) {
+            return Ok(106);
+        }
     }
+
     Ok(0)
 }
 
 fn main() {
-    let _ = App::new("cargo release")
+    let matches = App::new("cargo release")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Ning Sun <sunng@about.me>")
         .about("Cargo subcommand for you to smooth your release process.")
-        // TODO: .args_from_usage("")
+        .args_from_usage("
+        [dry-run]... --dry-run 'Donot actually change anything.'")
         .get_matches();
 
-    match execute() {
+    match execute(&matches) {
         Ok(code) => exit(code),
         Err(e) => {
             println!("Fatal: {}", e);
