e012b0f92d88cccaf7ac720ff6b887a550491322
Ning Sun <sunng@about.me>
2016-03-12 17:29:55 +0800

    (feat) guess relative path for subproject

--- src/git.rs
+++ src/git.rs
@@ -21,3 +21,11 @@ pub fn tag(name: &str, msg: &str) -> Result<bool, FatalError> {
 pub fn push() -> Result<bool, FatalError> {
     call(vec!["git", "push", "--follow-tags"])
 }
+
+pub fn top_level() -> Result<String, FatalError> {
+    let output = try!(Command::new("git")
+                      .arg("rev-parse")
+                      .arg("--show-toplevel")
+                      .output().map_err(FatalError::from));
+    String::from_utf8(output.stdout).map_err(FatalError::from)
+}
