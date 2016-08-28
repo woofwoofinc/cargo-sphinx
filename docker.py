89ebb5265ee243267279536853a2c3b1920691a5
Pascal Hartig <phartig@rdrei.net>
2016-08-28 14:22:05 +0100

    Add a docker helper script
    I still really dislike pasting stuff. I wrote this in Python because
    everyone and their dogs have it on their machines and it's easy to
    update and extend.

---
+++ docker.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+IMAGE_NAME = 'cargo-gh-pages'
+
+
+def has_image(name):
+    cmd = "docker images | awk '{{print $1}}' | grep '^{name}$' > /dev/null".format(
+            name=name),
+    proc = subprocess.run(cmd, shell=True)
+    return proc.returncode == 0
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', nargs='?',
+            help="Either 'shell' or 'docs'")
+
+    action = parser.parse_args().action
+
+    if not has_image(IMAGE_NAME):
+        run_build(IMAGE_NAME)
+
+    if action == 'shell':
+        run_shell(IMAGE_NAME)
+    elif action == 'docs':
+        run_docs(IMAGE_NAME)
+    else:
+        print("Unknown action '{}' specified.")
+
+def run_build(image):
+    cmd = "docker build -t {name} .".format(name=image)
+    subprocess.run(cmd, shell=True, check=True)
+
+def run_shell(image):
+    cmd = """docker run -it \\
+             -v "$(pwd):/{name}" \\
+             --workdir=/{name} \\
+             {name} \\
+             /bin/bash""".format(name=image)
+    subprocess.run(cmd, shell=True)
+
+def run_docs(image):
+    cmd = """docker run -it \\
+             -v "$(pwd):/{name}" \\
+             --workdir=/{name}/docs \\
+             {name} \\
+             make clean html""".format(name=image)
+    subprocess.run(cmd, shell=True)
+
+if __name__ == "__main__":
+    main()
