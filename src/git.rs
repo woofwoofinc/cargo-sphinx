d15b339837c4436b332af41bb880833dc412b5fa
Pascal Hartig <phartig@rdrei.net>
2017-11-26 22:55:47 +0000

    Run with legacy rustfmt

--- src/git.rs
+++ src/git.rs
@@ -5,13 +5,10 @@ use cmd::call;
 use failure::Error;
 
 pub fn remote_get_url(remote: &str) -> Result<String, Error> {
-    let output = try!(
-        Command::new("git")
-            .arg("remote")
-            .arg("get-url")
-            .arg(remote)
-            .output()
-    );
+    let output = try!(Command::new("git").arg("remote")
+                                         .arg("get-url")
+                                         .arg(remote)
+                                         .output());
 
     let url = try!(String::from_utf8(output.stdout));
     Ok(url)
@@ -25,27 +22,23 @@ pub fn add_all(dir: &str, shell: &mut Shell, dry_run: bool) -> Result<bool, Erro
     call(&["git", "add", "."], dir, shell, dry_run)
 }
 
-pub fn commit_all(
-    dir: &str,
-    msg: &str,
-    sign: bool,
-    shell: &mut Shell,
-    dry_run: bool,
-) -> Result<bool, Error> {
-    call(
-        &["git", "commit", if sign { "-S" } else { "" }, "-am", msg],
-        dir,
-        shell,
-        dry_run,
-    )
+pub fn commit_all(dir: &str,
+                  msg: &str,
+                  sign: bool,
+                  shell: &mut Shell,
+                  dry_run: bool)
+                  -> Result<bool, Error> {
+    call(&["git", "commit", if sign { "-S" } else { "" }, "-am", msg],
+         dir,
+         shell,
+         dry_run)
 }
 
-pub fn force_push(
-    dir: &str,
-    remote: &str,
-    refspec: &str,
-    shell: &mut Shell,
-    dry_run: bool,
-) -> Result<bool, Error> {
+pub fn force_push(dir: &str,
+                  remote: &str,
+                  refspec: &str,
+                  shell: &mut Shell,
+                  dry_run: bool)
+                  -> Result<bool, Error> {
     call(&["git", "push", "-f", remote, refspec], dir, shell, dry_run)
 }
